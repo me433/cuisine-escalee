37b4049f28a381f686fcf43bf2b82ecab2811818
Milo Wera <milowera@Milos-MacBook-Air.local>
2024-03-16 00:03:10 +0100

    basic app structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Nav from './Nav'
+import Content from './Content'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Content />
+    </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <nav>
+        <h1>Cuisine Escalée</h1>
+        <ul id={"nav"}>
+            <li>Recept toevoegen</li>
+            <li>Receptenboek</li>
+            <li>Zoeken placeholder</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
